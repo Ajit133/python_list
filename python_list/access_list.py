b43ae273ffbc8391817e88bb0b6925cd931f7ca6
Ajit133 <dasajit697@gmail.com>
2022-02-10 15:08:21 +0600

    update

--- python_list/access_list.py
+++ python_list/access_list.py
@@ -2,3 +2,5 @@ li = ["apple","banana","orange","papia"]
 print(li[-1])
 li = ["apple","banana","orange","papia"]
 print(li[0:3])
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[2:5])
