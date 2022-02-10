c57b89af88e33afe2b31db3acf4980afdc5766d8
Ajit133 <dasajit697@gmail.com>
2022-02-10 14:38:13 +0600

    list

---
+++ python_list/access_list.py
@@ -0,0 +1,2 @@
+li = ["apple","banana","orange","papia"]
+print(li[1:4])
---
+++ python_list/py_list.py
@@ -0,0 +1,18 @@
+# create a list
+thislist = ["apple","cherry","Banana"]
+print(thislist)
+
+# list allow duplicate value
+value = ["apple","cherry","banana","apple","cherry"]
+print(value)
+
+# use the len() function and we figure out the lis items 
+value = ["apple","cherry","banana","apple","cherry","Orange"]
+print(len(value))
+# A list can contain different data types:
+diffli = ["ajit",21,"Fulgazi","FCI"]
+print(diffli)
+
+# Using list constructor
+listcons = list(("bangla","English",13,17))
+print(listcons)
