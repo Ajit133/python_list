4cc452beb5b67206f803ee85eeaae108eac29eec
Ajit133 <dasajit697@gmail.com>
2022-02-11 19:08:14 +0600

    loop in list

---
+++ python_list/loop_list.py
@@ -0,0 +1,19 @@
+# using for loop in list 
+from re import I
+
+
+# thislist = ["apple","cheery","banana"]
+# for i in range(len(thislist)):
+#     print(thislist[i])
+
+# use while loop 
+# thislist = ["apple","cheery","banana","jackfruit","mango"]
+# i = 0
+# while i < len(thislist):
+#     print(thislist[i])
+#     i+=1
+# looping using list comprehension
+
+thislist = ["apple","cheery","orange","pineapple","jackfruits"]
+[print (x) for x in thislist]
+
