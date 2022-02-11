7a448dadf1371a705eb9e298e57d98ef5085f042
Ajit133 <dasajit697@gmail.com>
2022-02-11 11:43:02 +0600

    update list

--- python_list/access_list.py
+++ python_list/access_list.py
@@ -3,4 +3,14 @@ print(li[-1])
 li = ["apple","banana","orange","papia"]
 print(li[0:3])
 thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[:4])
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
 print(thislist[2:5])
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[:4])
+# rang of negative index
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[-4:-1])
+if "apple" in thislist:
+    print('"apple" is in the fruits list')
---
+++ python_list/change_listitems.py
@@ -0,0 +1,4 @@
+# chnage the list items
+li = ["apple","banana","jackfood","orange","pineapple","cherry"]
+li [1:3] = "balckcurrent","watermelon"
+print(li)
